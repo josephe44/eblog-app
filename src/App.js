c8b60939bf276bd865dd064e3ebc1237491d8d27
Osuohia Emmanuel <josephe442@gmail.com>
2022-05-30 21:17:30 +0100

    added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SiginIn from './pages/SignIn'
+import Register from './pages/Register'
+import Profile from './pages/Profile'
+import Categories from './pages/Categories'
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SiginIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/category" element={<Categories />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
