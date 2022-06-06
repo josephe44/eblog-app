2889aa1888d7454f963c1b7c58651f1c74bd072f
Osuohia Emmanuel <josephe442@gmail.com>
2022-06-06 17:47:36 +0100

    added feature for creating each blog

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from './pages/Register'
 import Profile from './pages/Profile'
 import Blog from './pages/Blog'
 import Categories from './pages/Categories'
+import CreatingBlog from './pages/CreatingBlog'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
@@ -13,13 +14,14 @@ function App() {
     <Router>
       <div className="flex flex-col justify-between h-full">
         <Navbar />
-        <main className="mx-auto pb-12">
+        <main className="pb-12 w-full">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/signin" element={<SiginIn />} />
             <Route path="/register" element={<Register />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/category" element={<Categories />} />
+            <Route path="/createBlog" element={<CreatingBlog />} />
             <Route
               path="/category/:categoryName/:blogName/:blogId"
               element={<Blog />}
