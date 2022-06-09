08467098a0e5fcadfa1967891ca0f49470605e95
Osuohia Emmanuel <josephe442@gmail.com>
2022-06-09 22:15:13 +0100

    change some styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,11 @@ function Home() {
     <div className="mb-10">
       <HomeBanner />
       <div className="container mx-auto">
-        <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-4 mx-4 mt-20">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 mx-4 mt-20">
           <div className="md:col-span-2 sm:col-span-1">
             <BlogCard />
           </div>
-          <div className='mt-6'>
+          <div className='mt-6 md:col-span-2 lg:col-span-1'>
             <CategoryCard />
           </div>
         </div>
